e7e9784b128c42babab0d6fd7772a75915b8fa6f
Iikka Timlin <iikka@docue.com>
2021-10-25 12:05:17 +0000

    Refactor collections, enforce types

--- src/Procountor/Collection/AbstractCollection.php
+++ src/Procountor/Collection/AbstractCollection.php
@@ -6,29 +6,38 @@ use Procountor\Procountor\Interfaces\AbstractResourceInterface;
 use ArrayIterator;
 use IteratorAggregate;
 use Countable;
-use TypeError;
 
 abstract class AbstractCollection implements IteratorAggregate, Countable
 {
+
+    /** @var array<AbstractResourceInterface> $items */
     private array $items = [];
 
-    abstract public function addItem(AbstractResourceInterface $item): AbstractCollection;
+    /**
+     * @template AbstractResource
+     * @param AbstractResource ...$items Pass in with spread operator
+     * @return void
+     */
+    abstract public function __construct(...$items);
 
-    public function addItemToCollection($item, $skipcheck = false)
-    {
-        if (!$skipcheck && !($item instanceof AbstractResourceInterface)) {
-            throw new TypeError('Invalid item');
-        }
-
-        $this->items[] = $item;
-        return $this;
-    }
+    /**
+     * Add item into collection
+     * @template AbstractResource
+     * @param AbstractResource $item
+     * @return self<AbstractResource>
+     */
+    abstract public function addItem($item): AbstractCollection;
 
     public function getIterator(): ArrayIterator
     {
         return new ArrayIterator($this->items);
     }
 
+    /**
+     * Get the count of items in the collection
+     *
+     * @return int
+     */
     public function count(): int
     {
         return $this->getIterator()->count();
